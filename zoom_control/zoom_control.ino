81895338f4ceaefd249057b94eebc0009d5fd3c9
Ron Hudson <3606628+pastorhudson@users.noreply.github.com>
2020-11-30 21:49:08 -0500

    Working LEDs

--- zoom_control/zoom_control.ino
+++ zoom_control/zoom_control.ino
@@ -14,6 +14,10 @@
 //*****************define your pins here***********************************************//
 #define button 16        // button on gpio16 D0
 #define joystick_pin 0 // analog pin used to connect the joystick
+#define top_green_pin 14
+#define top_red_pin 12
+#define bottom_green_pin 13
+#define bottom_red_pin 15
 
 //****************define the min, max and center you want for your joystick here*******//
 int val;                      // variable to read the value from the analog pin
@@ -22,9 +26,24 @@ int top_calibration = 1023;   // Top maximum value
 int bottom_calibration = 1; // Bottom minimum value
 int dead_zone = 4;            // How much play should the stick have before it reads above or below values
 
+
+
 Servo myservo;  // create servo object to control a servo
 
+struct Ledstate {
+  bool top_red = false;
+  bool top_green = false;
+  bool bottom_red = false;
+  bool bottom_green = false;
+}lstate;
+
+
 void setup() {
+  pinMode(top_red_pin, OUTPUT);
+  pinMode(top_green_pin, OUTPUT);
+  pinMode(bottom_red_pin, OUTPUT);
+  pinMode(bottom_green_pin, OUTPUT);
+  set_led_state();
   myservo.attach(4);  // attaches the servo on pin D2 to the servo object
   Serial.begin(115200);
 
@@ -66,7 +85,6 @@ void loop() {
     
   }
   
- 
   Serial.println(val);
   Serial.println("Old values are: "+String(data.center_calibration)+", "+String(data.top_calibration)+", "+String(data.bottom_calibration));
 
@@ -82,22 +100,49 @@ void loop() {
 
 bool calibrate () {
   
+  lstate.top_red = true;
+  lstate.top_green = false;
+  lstate.bottom_red = true;
+  lstate.bottom_green = false;
+  set_led_state();
   Serial.println("Please place stick at center");
   data.center_calibration = get_calibration();
   Serial.println("Center Calibration set: ");
   Serial.println(data.center_calibration);
+  
+  lstate.top_red = true;
+  lstate.top_green = false;
+  lstate.bottom_red = false;
+  lstate.bottom_green = false;
+  set_led_state();
   Serial.println("Please place stick at top");
   data.top_calibration = get_calibration();
   Serial.println("Top Calibration set: ");
   Serial.println(data.top_calibration);
+
+  lstate.top_red = false;
+  lstate.top_green = false;
+  lstate.bottom_red = true;
+  lstate.bottom_green = false;
+  set_led_state();
   Serial.println("Please place stick at bottom");
   data.bottom_calibration = get_calibration();
   Serial.println("Bottom Calibration set: ");
   Serial.println(data.bottom_calibration);
 
-//  if (center_calibration > bottom_calibration < top_calibration) {
-//    return true
-//  }
+  led_off();
+  lstate.bottom_green = true;
+  lstate.top_green = true;
+  set_led_state();
+
+  for (int i = 0; i < 7; i++) {    //take 100 readings
+  lstate.bottom_green = !lstate.bottom_green;
+  lstate.top_green = !lstate.top_green;
+  
+  set_led_state();
+  delay(300);
+  }
+
   data.calibrated = true;
   set_calibration(&data);
   return true;
@@ -106,13 +151,70 @@ bool calibrate () {
 
 int get_calibration() {
   int calibration = 0;    //reset the values
+
   delay(2500);
   Serial.print("calibrating position");
    for (int i = 0; i < 100; i++) {    //take 100 readings
     Serial.print(".");
+    if ( (i % 10) == 0) {
+      if (lstate.top_red) {
+        digitalWrite(top_red_pin, HIGH);
+      }
+      if (lstate.bottom_red) {
+        digitalWrite(bottom_red_pin, HIGH);
+      }
+    }
+    else {
+      digitalWrite(top_red_pin, LOW);
+      digitalWrite(bottom_red_pin, LOW);
+      digitalWrite(top_green_pin, LOW);
+      digitalWrite(bottom_green_pin, LOW);
+    }
     calibration += analogRead(joystick_pin);
     delay(5);
   }
   calibration /=  100;
   return calibration;
+}
+
+void set_led_state(){
+  for (int i = 0; i < 4; i++) {
+    if (lstate.top_red){      
+      digitalWrite(top_red_pin, HIGH);
+    }
+    else {
+      digitalWrite(top_red_pin, LOW);
+    }
+    
+    if (lstate.bottom_red){      
+      digitalWrite(bottom_red_pin, HIGH);
+    }
+    else {
+      digitalWrite(bottom_red_pin, LOW);
+    }
+    
+    if (lstate.top_green){      
+      digitalWrite(top_green_pin, HIGH);
+    }
+    else {
+      digitalWrite(top_green_pin, LOW);
+    }
+    
+    if (lstate.bottom_green){      
+      digitalWrite(bottom_green_pin, HIGH);
+    }
+    else {
+      digitalWrite(bottom_green_pin, LOW);
+    }
+
+  }
+  return;
+}
+
+void led_off() {
+  lstate.top_red = false;
+  lstate.bottom_red = false;
+  lstate.top_green = false;
+  lstate.bottom_green = false;
+  set_led_state();
 }
